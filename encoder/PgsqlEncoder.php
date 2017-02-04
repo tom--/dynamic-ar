270b23cb83c66cd145e16bcf8c92c5f1cdaf3aa2
nexus <nexus@localhost>
2017-02-04 22:45:08 +0200

    altered pgsql encoder to create attribute the way that seems proper

--- encoder/PgsqlEncoder.php
+++ encoder/PgsqlEncoder.php
@@ -19,16 +19,28 @@ class PgsqlEncoder extends BaseEncoder
      *
      * @return string a SQL expression
      */
-    public function dynamicAttributeExpression($name, $type = 'char')
+        public function dynamicAttributeExpression($name, $type=char)
     {
         $modelClass = $this->modelClass;
-        $sql = '[[' . $modelClass::dynamicColumn() . ']]';
-        foreach (explode('.', $name) as $column) {
-            $sql = "($sql->'$column' AS $type)";
-        }
+       // $sql = '[[' . $modelClass::dynamicColumn() . ']]';
+        $sql=str_replace(".",",",$name);
+//        foreach (explode('.', $name) as $column) {
+//           // $sql = "($sql->'$column' ::jsonb)";
+//            $sql=$name.',';
+//        }
 
-        return $sql;
+        return $modelClass::dynamicColumn().' #>\'{'.$sql.'}\'';
     }
+//    public function dynamicAttributeExpression($name, $type = 'char')
+//    {
+//        $modelClass = $this->modelClass;
+//        $sql = '[[' . $modelClass::dynamicColumn() . ']]';
+//        foreach (explode('.', $name) as $column) {
+//            $sql = "($sql->'$column' AS $type)";
+//        }
+//
+//        return $sql;
+//    }
 
     /**
      * Generates an SQL expression to select value of the dynamic column.
@@ -39,12 +51,13 @@ class PgsqlEncoder extends BaseEncoder
     {
         $modelClass = $this->modelClass;
         return '[[' . $modelClass::dynamicColumn() . ']]';
+       // return 'pipes';
     }
 
     /**
      * Creates a dynamic column SQL expression representing the given attributes.
      *
-     * @param array $attributes the dynamic attributes, which may be nested
+     * @param array $attributes the dynamic attributes, which may be json encoded
      *
      * @return null|\yii\db\Expression
      */
@@ -54,13 +67,13 @@ class PgsqlEncoder extends BaseEncoder
         }
 
         $params = [];
+
         // todo For now we only have Maria. Add PgSQL and generic JSON.
-       // static::encodeDynamicAttributeArray($attributes);
+         static::encodeDynamicAttributeArray($attributes);
        $sql = json_encode($attributes); //simply encode attributes
-       // $sql = static::dynColSqlMaria($attributes, $params);
+      // $sql = static::dynColSqlMaria($attributes, $params);
        $sql='\''.$sql.'\''; //simply add ' ' before and after so pg accepts the value
        return new \yii\db\Expression('(select CAST ('.$sql.' AS JSONB))', $params);
-       //return new \yii\db\Expression($sql,$params);
        
     }
 
@@ -94,8 +107,45 @@ class PgsqlEncoder extends BaseEncoder
 
         return $decoded;
     }
-
+    
     /**
+     * Creates the SQL and parameter bindings for setting dynamic attributes
+     * in a DB record as Dynamic Columns in Postgres.
+     *
+     * @param array $attrs the dynamic attributes, which may be nested
+     * @param array $params expression parameters for binding, passed by reference
+     *
+     * @return string SQL for a DB Expression
+     * @throws \yii\base\Exception
+     */
+    private static function dynColSqlMaria(array $attrs, & $params)
+    {
+        $sql = [];
+        foreach ($attrs as $key => $value) {
+            if (is_object($value) && !($value instanceof ValueExpression)) {
+                $value = method_exists($value, 'toArray') ? $value->toArray() : (array) $value;
+            }
+            if ($value === [] || $value === null) {
+                continue;
+            }
+            $phKey = static::placeholder();
+            $phValue = static::placeholder();
+            $sql[] = $phKey;
+            $params[$phKey] = $key;
+            if ($value instanceof ValueExpression || is_float($value)) {
+                $sql[] = $value;
+            } elseif (is_scalar($value)) {
+                $sql[] = $phValue;
+                $params[$phValue] = $value;
+            } elseif (is_array($value)) {
+                $sql[] = static::dynColSqlMaria($value, $params);
+            }
+        }
+       return $sql === [] ? 'null' : 'json_build_object(' . implode(',', $sql) . ')::jsonb';
+    //  return $sql === [] ? 'null' : '[' . implode(',', $sql) .']';
+       // return json_encode($sql);
+    }
+        /**
      * Creates the SQL and parameter bindings for setting dynamic attributes
      * in a DB record as Dynamic Columns in Maria.
      *
@@ -105,7 +155,65 @@ class PgsqlEncoder extends BaseEncoder
      * @return string SQL for a DB Expression
      * @throws \yii\base\Exception
      */
-//    private static function dynColSqlPostgres(array $attrs, & $params)
+//        private static function jsonbColSqlPg(array $attrs, & $params)
+//    {
+//        $sql = [];
+//       $sql[]=$attrs;
+//                
+//        foreach ($attrs as $value) 
+//            {
+//             if (is_array($value))
+//             {
+//                 foreach($value as $val)
+//                  { do 
+//                      if (is_array($val))
+//                   {
+//                     $val=array_shift($val);
+//                     $sql[]=json_encode($val);
+//                   }
+//                   else {$sql[]=$val;}
+//                   while (is_array($val));
+//                 }
+//                 
+//             }
+//            
+//            else
+//             {
+//             $sql[]=$value;
+//             }
+//            }
+        
+//            if (is_object($value) && !($value instanceof ValueExpression)) {
+//                $value = method_exists($value, 'toArray') ? $value->toArray() : (array) $value;
+//            }
+//            if ($value === [] || $value === null) {
+//                continue;
+//            }
+//
+////            $phKey = static::placeholder();
+////            $phValue = static::placeholder();
+////            $sql[] = $phKey;
+////            $params[$phKey] = $key;
+//
+//            if ($value instanceof ValueExpression || is_float($value)) {
+//                $sql[] = $value;
+//            } elseif (is_scalar($value)) {
+//                $sql[] = $phValue;
+//                $params[$phValue] = $value;
+//            } elseif (is_array($value)) {
+//                $sql[] = static::dynColSqlMaria($value, $params);
+//            }
+//        }
+        // $sql=  json_encode($sql);
+    //   $sql=  json_encode(array_values($sql));
+      // $sqlval=new \stdClass();
+     //  $sql=  array_shift($sql);
+     // $sql= array_shift($sql);
+//      $sqlval=(object) array($sql);
+//      $sqlexpr=  json_encode($sqlval);
+//        return $sql === [] ? 'null' : $sqlexpr;
+//    }
+//    private static function dynColSqlMaria(array $attrs, & $params)
 //    {
 //        $sql = [];
 //        foreach ($attrs as $key => $value) {
@@ -127,7 +235,7 @@ class PgsqlEncoder extends BaseEncoder
 //                $sql[] = $phValue;
 //                $params[$phValue] = $value;
 //            } elseif (is_array($value)) {
-//                $sql[] = static::dynColSqlPostgres($value, $params);
+//                $sql[] = static::dynColSqlMaria($value, $params);
 //            }
 //        }
 //
